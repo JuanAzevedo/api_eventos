2f55838c369137a48fcf712a8bc33c3ba60f941a
SeuNome <seuemail@example.com>
2024-07-12 20:21:23 -0300

    pasta 34

--- schemas.py
+++ schemas.py
@@ -3,9 +3,9 @@ from datetime import datetime, date
 
 # Schema para criar um novo evento
 class EventoCreateSchema(BaseModel):
-    nome: str
-    cidade: str
-    data_evento: str
+    nome: str = 'Rock in rio'
+    cidade: str = 'Rio de Janeiro'
+    data_evento: str = '2024-11-11'
 
     @validator('data_evento')
     def check_date_format(cls, value):
@@ -19,10 +19,10 @@ class EventoCreateSchema(BaseModel):
 
 # Schema para atualizar um evento existente
 class EventoUpdateSchema(BaseModel):
-    id: int
-    nome: str
-    cidade: str
-    data_evento: str
+    id: int = 1
+    nome: str = 'Isso não é uma festa'
+    cidade: str = 'São paulo'
+    data_evento: str = '2024-12-12'
 
     @validator('data_evento')
     def check_date_format(cls, value):
@@ -43,7 +43,7 @@ class EventoViewSchema(BaseModel):
 
 # Schema para deletar um evento
 class EventoDeleteSchema(BaseModel):
-    id: int
+    id: int = 1
 
 # Schema para mensagens de erro
 class ErrorSchema(BaseModel):
